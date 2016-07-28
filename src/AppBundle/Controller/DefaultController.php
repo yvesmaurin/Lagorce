2daa23ef0fdfeaa6f28fbb38c8f87ae0653f1118
yvesmaurin <yves.maurin51@gmail.com>
2016-07-28 16:43:02 +0200

    add guestbook

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\GuestBook;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,10 +16,10 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig'
-            /* Paramètres de la page About de Symfony. Comment par Vincent le 13/7/16
-            , [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ] */
+        /* Paramètres de la page About de Symfony. Comment par Vincent le 13/7/16
+        , [
+        'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+    ] */
         );
     }
 
@@ -45,8 +46,12 @@ class DefaultController extends Controller
      */
     public function contactAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/contact.html.twig');
+        $repo = $this->getDoctrine()->getRepository('AppBundle:GuestBook');
+        $guestBooks = $repo->findAll();
+
+        return $this->render('default/contact.html.twig', array(
+            'guestBooks' => $guestBooks
+        ));
     }
 
     /**
@@ -56,5 +61,38 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/notice.html.twig');
+    }
+
+    /**
+     * @Route("/contact/add", name="contact_add")
+     */
+    public function addContactAction(Request $request)
+    {
+        if ($request->getMethod() == "POST") {
+            $em = $this->getDoctrine()->getManager();
+
+            $guestBook = new GuestBook();
+            $guestBook->setNom($request->get('nom'));
+            $guestBook->setCommentaire($request->get('commentaire'));
+
+            $em->persist($guestBook);
+            $em->flush();
+
+            $sujet= 'Un nouveau commentaire à propos du site';
+            $message = 'De ' . $request->get('nom') . ' :\n' . $request->get('commentaire');
+            $message .= "\n La validation se fera sur la page d'administration du site.";
+            $destinataire = "vi.berniere@gmail.com";
+            $headers = "From: <jean-pierre.ozil@liondor-lagorce.fr>\n";
+            $headers .= "Content-Type : text/plain; charset = 'utf-8'";
+
+            if (mail($destinataire, $sujet, $message, $headers)) {
+                $this->addFlash('message', 'Votre commentaire a bien été envoyé');
+            } else {
+                $this->addFlash('message', "Votre commentaire n'a pas pu être envoyé, recommencez ultérieurement, merci");
+
+            }
+        }
+
+        return $this->redirect($this->generateUrl('contact'));
     }
 }
---
+++ src/AppBundle/Entity/GuestBook.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * GuestBook
+ *
+ * @ORM\Table(name="guest_book")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\GuestBookRepository")
+ */
+class GuestBook
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetimetz")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="commentaire", type="text")
+     */
+    private $commentaire;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="visible", type="boolean")
+     */
+    private $visible;
+
+    public function __construct()
+    {
+        $this->visible = false;
+        $this->date = new \DateTime();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return GuestBook
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return GuestBook
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set commentaire
+     *
+     * @param string $commentaire
+     *
+     * @return GuestBook
+     */
+    public function setCommentaire($commentaire)
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    /**
+     * Get commentaire
+     *
+     * @return string
+     */
+    public function getCommentaire()
+    {
+        return $this->commentaire;
+    }
+
+    /**
+     * Set visible
+     *
+     * @param boolean $visible
+     *
+     * @return GuestBook
+     */
+    public function setVisible($visible)
+    {
+        $this->visible = $visible;
+
+        return $this;
+    }
+
+    /**
+     * Get visible
+     *
+     * @return bool
+     */
+    public function getVisible()
+    {
+        return $this->visible;
+    }
+}
+
